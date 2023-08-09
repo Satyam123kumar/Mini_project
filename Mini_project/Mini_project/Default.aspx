275be2f88e3868fd11df86e22ec5650834275abd
Satyam123kumar <kumar.149@iitj.ac.in>
2023-08-09 13:19:16 +0530

    satyam

---
+++ Mini_project/Mini_project/Default.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Mini_project.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 26px;
+        }
+        .auto-style2 {
+            height: 26px;
+            width: 442px;
+        }
+        .auto-style3 {
+            width: 442px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Find a Blood Donor</div>
+        <table style="width:100%;">
+            <tr>
+                <td class="auto-style2">Blood Group&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+                <td class="auto-style1"></td>
+                <td class="auto-style1"></td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Select State&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:DropDownList ID="DropDownList2" runat="server" Width="185px" CssClass="form-control" AutoPostBack="True"                            OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">                         </asp:DropDownList>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Select District&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:DropDownList ID="DropDownList3" runat="server" Width="185px" CssClass="form-control" AutoPostBack="True"                            OnSelectedIndexChanged="DropDownList3_SelectedIndexChanged">                         </asp:DropDownList>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Select City&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:DropDownList ID="DropDownList4" runat="server">
+                    </asp:DropDownList>
+                    <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Search" />
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
+
+
---
+++ Mini_project/Mini_project/Default.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace Mini_project
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        public partial class donorRegistration : System.Web.UI.Page
+        {
+
+            private List<string> bloodTypes = new List<string>
+        {
+            "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"
+        };
+
+            private Dictionary<string, Dictionary<string, List<string>>> indianCities = new Dictionary<string, Dictionary<string, List<string>>>
+        {
+            {
+                "Andhra Pradesh", new Dictionary<string, List<string>>
+                {
+                    { "Anantapur", new List<string> { "Anantapur City", "Dharmavaram", "Hindupur" } },
+                    { "Chittoor", new List<string> { "Chittoor", "Tirupati", "Madanapalle" } },
+                    // ... Add more districts and cities as needed
+                }
+            },
+            {
+                "Arunachal Pradesh", new Dictionary<string, List<string>>
+                {
+                    { "Tawang", new List<string> { "Tawang Town", "Bomdila" } },
+                    { "West Kameng", new List<string> { "Bhalukpong", "Bomdila" } },
+                    // ... Add more districts and cities as needed
+                }
+            },
+            // ... Add more states and districts as needed
+        };
+
+            private List<string> availableTypes = new List<string>
+        {
+            "Available", "Unavailable"
+        };
+
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                if (!IsPostBack)
+                {
+                    DropDownList1.DataSource = bloodTypes;
+                    DropDownList1.DataBind();
+
+                    DropDownList2.DataSource = indianCities.Keys;
+                    DropDownList2.DataBind();
+
+                    DropDownList5.DataSource = availableTypes;
+                    DropDownList5.DataBind();
+                }
+            }
+
+            protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+            {
+                string selectedState = DropDownList2.SelectedValue;
+                if (indianCities.ContainsKey(selectedState))
+                {
+                    DropDownList2.DataSource = indianCities[selectedState].Keys;
+                    DropDownList2.DataBind();
+                }
+            }
+
+            protected void DropDownList3_SelectedIndexChanged(object sender, EventArgs e)
+            {
+                string selectedState = DropDownList2.SelectedValue;
+                string selectedDistrict = DropDownList2.SelectedValue;
+
+                if (indianCities.ContainsKey(selectedState) && indianCities[selectedState].ContainsKey(selectedDistrict))
+                {
+                    DropDownList3.DataSource = indianCities[selectedState][selectedDistrict];
+                    DropDownList3.DataBind();
+                }
+            }
+
+
+            protected void Button1_Click(object sender, EventArgs e)
+        {
+            string blood_group = Convert.ToString(DropDownList1.Text);
+            string country = Convert.ToString(TextBox2.Text);
+            string state = Convert.ToString(TextBox3.Text);
+            string district = Convert.ToString(TextBox4.Text);
+            string city = Convert.ToString(TextBox5.Text);
+
+            MySqlConnection conn = new MySqlConnection("host=localhost;username=root;password=Satyam@123;database=");
+
+            conn.Open();
+
+            string query = $"search product values ({blood_group}, '{country}', {state}, '{district}', '{city}');";
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+
+            }
+
+
+            /*Response.Write("<script> alert('Blood donar found');</script>");
+*/
+        }
+    }
+}
